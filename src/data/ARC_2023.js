3a144c744e0c74c039e18cac2f651d65482b2983
Strocs <strocsdev@gmail.com>
2023-01-12 00:07:20 -0300

    Add data

---
+++ src/data/ARC_2023.js
@@ -0,0 +1,14 @@
+export const ARC_2023 = {
+  title: 'Festival ARC 2023',
+  location: 'La Serena, Chile.',
+  days: '9, 10, 11 y 12 de Febrero',
+  description:
+    'La nueva versión del Festival de las Artes Región de Coquimbo busca incidir en la forma en la que la comunidad habita su ciudad, a través de un nuevo formato que transformará los espacios públicos en un circuito artístico caminable, en el que convergerá la curiosidad del ciudadano de a pie con la belleza presente en las distintas disciplinas que conforman el sector artístico de nuestra región.',
+  webSite: 'https://festivalarc.cl',
+  socialMedia: {
+    instagram: 'https://www.instagram.com/festival_arc',
+    facebook: 'https://www.facebook.com/FestivalARC',
+    twitter: 'https://twitter.com/FestivalARC',
+    email: ''
+  }
+}
