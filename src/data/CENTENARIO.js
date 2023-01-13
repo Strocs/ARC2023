3ae550c13576884feae54de66585e89bec63ff6c
Strocs <strocsdev@gmail.com>
2023-01-12 23:56:06 -0300

    Add data

---
+++ src/data/CENTENARIO.js
@@ -0,0 +1,90 @@
+export const CENTENARIO = {
+  locationName: 'Teatro Centenario',
+  color: 'centenario',
+  image: '',
+  artists: [
+    {
+      name: 'Camila Moreno',
+      socialMedias: {
+        webSite: 'https://www.camila-moreno.com/',
+        instagram: 'https://www.instagram.com/camila_moreno_',
+        facebook: 'https://www.facebook.com/camilamorenoficial'
+      },
+      p1: 'El sentido del riesgo es la característica distintiva de la cantante, autora y compositora Camila Moreno, expresada en un acercamiento personal a los sonidos acústicos, a la electricidad del rock y la producción electrónica. Ha conseguido difusión internacional, buscando un vínculo entre su cantautoría y procesos sociales más amplios, situándose entre las voces destacadas de una generación de cantantes chilenos que comparten una inclinación a la raíz folclórica por instinto y un canto personal y testimonial al mismo tiempo.',
+      p2: 'Antes de iniciar su camino como solista, la artista participó de distintos proyectos, como Princesa Leia Project, junto al músico y productor, Gerardo Figueroa (2003 – 2005), el dúo Caramelitus, en el que trabajó con el músico Tomás Preuss (2006) y las bandas de raíz latinoamericana Cúchara (2006-2008) y Las Polleritas (2007).',
+      p3: 'Tras su debut solista con el trabajo Almismotiempo (2009), la artista comenzó a ganar notoriedad, tanto en Chile como en el extranjero, con grandes hitos como la nominación de su tema “Millones” en la categoría “mejor canción alternativa” de los Latin Grammy del año 2009; presentaciones en grandes escenarios de la música nacional, como el Festival del Huaso del Olmué en 2010 y el Festival de la Canción de Viña del Mar en 2011; su primera gira por Europa en 2012, con presentaciones en algunas de las ciudades más importantes del continente, como Berlín, Barcelona y París y su debut en los festivales Lollapalooza Chile y Rock al Parque de Colombia en el mismo año.',
+      extra: {
+        text: 'Su discografía en estudio como artista en solitario está compuesta por los siguientes trabajos:',
+        list: [
+          'Almismotiempo (2009)',
+          'Panal (2012)',
+          'Mala Madre (2015)',
+          'Rey (2021)'
+        ]
+      }
+    },
+    {
+      name: 'Ases Falsos',
+      socialMedias: {
+        webSite: 'https://asesfalsos.cl',
+        instagram: 'https://www.instagram.com/asesfalsos',
+        facebook: 'https://www.facebook.com/asesfalsos'
+      },
+      p1: 'Ases Falsos es una de las bandas ícono del rock alternativo chileno actual, integrada por Cristóbal Briceño, Simón Sánchez, Martín del Real, Francisco Rojas, Sergio Sanhueza y Hermes Villalobos.',
+      p2: 'El proyecto nace en el año 2011, a raíz del final simbólico de la banda Fother Muckers, hecho que generó una nueva era musical de la banda, encarnada a través del nombre “Ases Falsos”, cuyo origen se encuentra en el track 10 del disco “Justo y Necesario” de la primera, titulado “Los Ases Falsos”. Este cambio buscaría la evolución y madurez del estilo musical del grupo, pero manteniendo la esencia que había trabajado desde el año 2005.',
+      p3: 'Su disco debut, Juventud Americana, estrenado el 3 de septiembre de 2012, fue elegido el mejor disco chileno de la década por Culto, la sección de arte y espectáculos del medio nacional La Tercera. La banda se caracteriza por la frescura de su sonido y la mezcla entre diferentes estilos musicales que convergen en un proyecto sólido, que se ha expresado en una variada discografía, compuesta por los discos Juventud Americana (2012), Conducción (2014), El Hombre Puede (2016), Mala Fama (2018), Tacto (2020) y Chocadito (2020).'
+    },
+    {
+      name: 'Marineros',
+      socialMedias: {
+        instagram: 'https://www.instagram.com/omarineros',
+        facebook: 'https://www.facebook.com/omarineros'
+      },
+      p1: 'Citado por el diario español El País como una de las promesas musicales latinoamericanas del 2014 y apadrinadas musicalmente por Javiera Mena, una de las exponentes del pop chileno más importantes de las últimas décadas, Marineros, proyecto conformado por la cantante Constanza Espina y la guitarrista Soledad Puentes, ha desarrollado una poderosa carrera en el circuito musical nacional e internacional a través de su obra, definida por sus mismas autoras como un proyecto de “pop existencial”.',
+      p2: 'El proyecto debutó en el año 2015 con el álbum “O Marineros”, trabajo producido por Cristián Heyne, compositor y productor que ha desarrollado su carrera trabajando con artistas como Alex Anwandter, Javiera Mena, Gepe y Fernando Milagros, entre otros. Sobre este trabajo, el dúo expresó en distintas entrevistas a medios nacionales que este trabajo “son nuestros corazones traducidos en canciones. Lo escuchamos y se vuelve parte de nuestro flujo sanguíneo y brama al ritmo de nuestro palpitar (…)”.',
+      p3: 'Además, cuentan con distintos sencillos como Torbellino (2016), Ámbar (2019), Todo a la Vez (2020), Rosas (2021), A Toda Velocidad (2022) y Loco (2022).'
+    },
+    {
+      name: 'Yorka',
+      socialMedias: {
+        instagram: 'https://www.instagram.com/yorkayork',
+        facebook: 'https://www.facebook.com/yorkaoficial'
+      },
+      p1: 'Yorka es un dúo pop chileno conformado por las hermanas Yorka y Daniela Pastenes, quienes, a través de su música, han logrado un espacio en la escena musical nacional, con cuatro discos de larga duración editados a la fecha: Imperio (2015), Humo (2018), MAL (2020) y Chao (2022).',
+      p2: 'El proyecto, formado a principios del año 2012, se caracteriza por su variedad de sonidos, desde melodías más bailables, hasta baladas propias del folk, acompañados de potentes letras que engloban reflexiones sobre la pérdida, el amor y el inevitable paso del tiempo.',
+      p3: 'Dentro de los hitos de este proyecto está su participación en el festival nacional Ruidosa (2017), donde compartió escenario con artistas como Francisca Valenzuela, Nicole, Mariel Mariel, Entrópica y Carolina Nissen, entro otras; su presentación en 2021 en el festival norteamericano South by Southwest (SXSW); su actuación en la versión 2022 de Lollapalooza Chile y su posterior gira por Argentina, en la que tocaron ante dos mil personas en el Centro Cultural Kirchner de Buenos Aires.'
+    },
+    {
+      name: 'Flamäntes',
+      p1: 'anda chilena de indie pop, que nace en 2019 como un proyecto conformado por reconocidos músicos de la región de Coquimbo, con marcada influencia en el soul y el funk de los 70`s, Flamantës presenta un espectáculo ecléctico y bailable.',
+      p2: 'Recientemente, Flamantës representó a la región de Coquimbo en el festival Rockódromo 2022 con gran recibimiento por parte del público en Valparaíso, donde dieron inicio a la gira de lanzamiento de su nuevo disco “Tres tristes tigres”, presentando entre otros, “La ciudad de las bestias” y “Fulgor”, ambos singles y videoclips inspirados en la poesía y literatura latinoamericana exploran la proyección del fantasma del espíritu siendo esta la marca visual del nuevo álbum.',
+      p3: 'Actualmente la banda se prepara para el lanzamiento de “Máscara”, tercer sencillo de Tres tristes tigres. Flamantës está conformado por Zacarías Pastén, Francisco Pinto, Cristian Vera y Esteban Guzmán.'
+    },
+    {
+      name: 'Pájarxs de Tierra',
+      socialMedias: {
+        instagram: 'https://www.instagram.com/pajarxsdetierra/'
+      },
+      p1: 'Pájarxs de Tierra es un proyecto musical de la cuarta región, que comienza con la cantautora Roxana Ledezma quien interpretaba sus canciones de manera solista para luego integrar a músicos/amigos de la escena artística serenense y añadir mejores componentes sonoros al lenguaje musical, con influencias provenientes del rap, soul, funk, rock, y así conformando oficialmente una agrupación musical con composiciones propias. ',
+      p2: 'El proyecto está compuesto por Roxana Ledezma (voces y guitarra), Jean Tabilo (guitarra y vientos), Carlos Milla (bajo) y Nicolás Guerrero (batería)',
+      p3: 'El grupo estrenó en enero de 2022 su primer videoclip de la canción “Violencia” y en abril del mismo año, lanzaron su segundo material audiovisual llamado “Falsa Modestia” En la actualidad se encuentran finalizando la grabación de su primer EP titulado “Violencia".'
+    },
+    {
+      name: 'Sequía',
+      p1: 'Sequía nace el año 2015 en la ciudad de Coquimbo. Su sonido se inclina por el Post-Rock, el Post-Hardcore y el Rock Progresivo, aunque muchas veces bordean lo más experimental. Sus canciones son reflejo de las diversas influencias aportadas por cada uno de sus integrantes. ',
+      p2: 'En sus inicios (2015), se presentaron en distintos lugares y festivales en las ciudades de Coquimbo y La Serena. En el año 2016 - 2017, participan en festivales en Santiago, además de comenzar la grabación de su primer EP en Estudio 19, en Coquimbo, proceso que terminó en 2018.',
+      p3: 'En ese año, Sequía cambia de formación, lo cual genera el cierre de un ciclo y el cambio en su sonoridad, en donde también se incorporan nuevos elementos como Kaoss Pad y Loops en vivo, con lo que buscan lograr un sonido sólido y propio.',
+      p4: 'En el año 2019, se lanzó en formato digital y físico su primer trabajo de estudio titulado ‘’Sentidos’’. Durante ese año, son partícipes del festival cultural y medioambiental ‘’Chango Rock Fest II’’ y del Día de La Música Chilena en la Plaza de Armas de la ciudad de La Serena, compartiendo escenario con destacados artistas nacionales y regionales.',
+      p5: 'En el año 2021, la banda forma parte del festival Rockódromo junto a grupos regionales, y en 2022, se presentan en el festival Astromúsica en el Teatro Municipal de Ovalle, instancia en que Sequía estrena canciones en su nuevo formato como banda, donde incorporan una guitarra eléctrica adicional y una serie de sintetizadores que le dan un carácter más experimental a su música. En la actualidad, Sequía se encuentra produciendo su nuevo material, el cual se estrenará en verano de 2023.',
+      extra: {
+        text: 'Sequía es:',
+        list: [
+          'Sebastián Torres - Composición, voz principal, guitarra eléctrica y sintetizador.',
+          'Juan Carlos Ossandón - Composición, coros, bajo eléctrico y sintetizadores.',
+          'Alfredo Bahamondes - Composición, coros y guitarra eléctrica.',
+          'Jorge Veas - Composición, batería y diseño gráfico.'
+        ]
+      }
+    }
+  ]
+}
