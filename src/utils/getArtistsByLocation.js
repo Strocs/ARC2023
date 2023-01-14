3a9013f2712a28790598ffc9c52ef30ed5bc9cc7
Strocs <strocsdev@gmail.com>
2023-01-14 16:03:57 -0300

    Add get artists by query

---
+++ src/utils/getArtistsByLocation.js
@@ -0,0 +1,7 @@
+import { ARC_2023 } from '../data/ARC_2023'
+
+const { locations } = ARC_2023
+
+export function getArtistsByLocation(query) {
+	return locations.find(location => location.id === query).artists.map(artist => artist.name)
+}
