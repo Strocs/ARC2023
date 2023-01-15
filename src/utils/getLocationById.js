662a5d6716fb01e5e170cba3c6318289b4f732dc
Strocs <strocsdev@gmail.com>
2023-01-15 19:07:27 -0300

    Add get locations by id util

---
+++ src/utils/getLocationById.js
@@ -0,0 +1,7 @@
+import { ARC_2023 } from '../data/ARC_2023'
+
+const { locations } = ARC_2023
+
+export function getLocationById(query) {
+	return locations.find(location => location.id === query)
+}
