734dca2fa7ae42293ef06f9251852140f97362e5
Strocs <strocsdev@gmail.com>
2023-01-31 15:01:57 -0300

    Add robots.txt and sitemap generator

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,11 @@ import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 import tailwind from '@astrojs/tailwind'
+// https://astro.build/config
+import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind()]
+	site: 'https://festivalarc.cl',
+	integrations: [tailwind(), sitemap()]
 })
