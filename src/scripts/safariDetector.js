69657083690e9b9cbd7b8d8c4c123321b8f15830
Strocs <strocsdev@gmail.com>
2023-02-03 15:17:40 -0300

    Add: safari mobile alert

--- src/scripts/safariDetector.js
+++ src/scripts/safariDetector.js
@@ -1,7 +1,14 @@
+const isIphone = /iPhone/.test(navigator.userAgent)
 const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent)
 const safariVersion = isSafari && /Version\/([0-9.]+) Safari/.exec(navigator.userAgent)[1]
 
-if (isSafari && parseFloat(safariVersion) <= 14) {
+if (isIphone && isSafari && parseFloat(safariVersion) <= 12) {
+	document.write(
+		`<p>Notamos que estas usando la versión de Safari ${safariVersion}. Para una mejor experiencia, considera actualizar tu navegador.</p>`
+	)
+}
+
+if (isSafari && parseFloat(safariVersion) <= 13) {
 	alert(
 		`Notamos que estas usando la versión de Safari ${safariVersion}. Para una mejor experiencia, considera actualizar tu navegador.`
 	)
